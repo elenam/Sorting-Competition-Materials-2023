b12acb15c10232f7e0e89a73ad0250fc3ab7346d
Elena M <elenam@morris.umn.edu>
2023-10-11 12:23:51 -0500

    Moved Group 0 into Group 00, added .class files for the faster Group 0

--- src/Group00.java
+++ src/Group00.java
@@ -12,7 +12,7 @@ import java.util.Scanner;
 // To avoid file reading/writing connections to the server, run in /tmp 
 // of your lab machine.
 
-public class Group0 {
+public class Group00 {
 
 	public static void main(String[] args) throws InterruptedException, FileNotFoundException {
 
